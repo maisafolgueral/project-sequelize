d797c29adc47dd36176f8a47a49f0648d72fbfaa
Maisa <maisa.folgueral@gmail.com>
2023-04-10 18:24:53 -0300

    Updated values sequelize

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 (async () => {
   const Planet = require("./models/Planet");
 
+  const updatePlanets = await Planet.findByPk(4);
+  updatePlanets.name = "Jupiter";
+  await updatePlanets.save();
+
   /* Criação de planetas no banco
     const newPlanet = await Planet.create({
     name: "Venus",
     position: 5,
   });
-  */
 
-  const seePlanets = await Planet.findAll({
+    const seePlanets = await Planet.findAll({
     where: {
       name: "Terra",
     },
   });
+  */
 
-  console.log(seePlanets);
+  console.log(updatePlanets);
 })();
