12c342e1ab8c1aa6438b7c6d5bdaeffd00f9cc4c
Maisa <maisa.folgueral@gmail.com>
2023-04-10 17:47:39 -0300

    Consult values sequelize

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 (async () => {
   const Planet = require("./models/Planet");
 
-  const newPlanet = await Planet.create({
+  /* Criação de planetas no banco
+    const newPlanet = await Planet.create({
     name: "Venus",
     position: 5,
   });
+  */
 
-  console.log(newPlanet);
+  const seePlanets = await Planet.findAll({
+    where: {
+      name: "Terra",
+    },
+  });
+
+  console.log(seePlanets);
 })();
