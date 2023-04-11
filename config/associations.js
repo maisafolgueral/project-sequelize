da317b1d0a480b064b6a6da5e3b07d56ee6e7d1b
Maisa <maisa.folgueral@gmail.com>
2023-04-11 16:49:44 -0300

    Created association Many To Many

--- config/associations.js
+++ config/associations.js
@@ -1,12 +1,27 @@
 const Planet = require("../models/Planet");
 const Satellite = require("../models/Satellite");
 
+const Cap = require("../models/Cap");
+const Spaceship = require("../models/Spaceship");
+
 /*
-Planet.hasOne(Satellite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
+Planet.hasMany(Satellite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
 Satellite.belongsTo(Planet, { foreingKey: "planetId", as: "planet" });
 */
 
-Planet.hasMany(Satellite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
+Planet.hasOne(Satellite, { onDelete: "CASCADE", onUpdate: "CASCADE" });
 Satellite.belongsTo(Planet, { foreingKey: "planetId", as: "planet" });
+
+Cap.belongsToMany(Spaceship, {
+  foreingKey: "capId",
+  through: "capSpaceship",
+  as: "spaceships",
+});
+
+Spaceship.belongsToMany(Cap, {
+  foreingKey: "spaceshipId",
+  through: "capSpaceship",
+  as: "caps",
+});
 
 module.exports = { Planet, Satellite };
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const routes = express.Router();
 
 const PlanetController = require("../Controller/PlanetController");
 const SatelliteController = require("../Controller/SatelliteController");
+const CapController = require("../Controller/CapController");
+const SpaceshipController = require("../Controller/SpaceshipController");
 
 // Rotas de Planets
 routes.post("/planets", PlanetController.store);
@@ -13,5 +15,13 @@ routes.delete("/planets/:id", PlanetController.delete);
 // Rotas de Associações
 routes.post("/planet/:planetId/satellites", SatelliteController.store);
 routes.get("/planet/:planetId/satellites", SatelliteController.index);
+
+// Rotas de Cap
+routes.post("/cap", CapController.store);
+routes.get("/cap", CapController.index);
+
+// Rotas de SpaceShip
+routes.post("/caps/:capId/spaceships", SpaceshipController.store);
+routes.get("/caps/:capId/spaceships", SpaceshipController.index);
 
 module.exports = routes;
