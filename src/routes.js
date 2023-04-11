0dab9054a8148d0b640981e9425811d931ecd481
Maisa <maisa.folgueral@gmail.com>
2023-04-11 14:39:07 -0300

    Changed for API

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const routes = express.Router();
+
+const PlanetController = require("../Controller/PlanetController");
+
+// Rotas de Planets
+routes.post("/planets", PlanetController.store);
+routes.get("/planets", PlanetController.index);
+routes.put("/planets/:id", PlanetController.put);
+routes.delete("/planets/:id", PlanetController.delete);
+
+module.exports = routes;
